f6bfea9668a50144619d4c61b02f1a1c75c1ad71
Stephan Lefort <lefortsge@gmail.com>
2025-04-29 15:19:06 -0700

    Create a pool connection to the database

---
+++ server/config/db.js
@@ -0,0 +1,14 @@
+import pkg from 'pg';
+import env from 'dotenv';
+
+env.config();
+
+const { Pool } = pkg;
+
+// Connection pool to database
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false} : false, // it enable SSL and disable certificate verification in production
+});
+
+
