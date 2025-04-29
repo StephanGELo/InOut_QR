5294895d95dc8ce8ae7d561e6b569d842c3b8542
Stephan Lefort <lefortsge@gmail.com>
2025-04-28 18:56:53 -0700

    Server is up and running

--- server/server.js
+++ server/server.js
@@ -2,32 +2,31 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import pg from 'pg';
 import env from 'dotenv';
 
-// Import routes
+// Import routes 
 
 // Initialize express app
 const app = express();
 
-//load enviroment variables
+// Load environment variables
 env.config();
 
 // Middleware
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(cors()); // <-- Allow frontend to connect to backend
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); //  for POST requests
 app.use(express.static('public'));
 
-//  Health check route
-app.get('/', (req, res) =>{
-  res.send('INOut QR is running ok');
+// Health check route
+app.get('/', (req, res) => {
+  res.send('Server is up and running!');
 });
 
-// Use routes
+// Use routes (to be added later)
 
-
-// start server
-const PORT = process.env.PORT || 3000;
+// Start server
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
