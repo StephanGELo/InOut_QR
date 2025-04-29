32b679684ddf47844023f1839fbbdb894c15b67d
Stephan Lefort <lefortsge@gmail.com>
2025-04-29 15:58:18 -0700

    Test connection to the Database and result is successful

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import env from 'dotenv';
+import pool from './config/db.js';
+
 
 // Import routes 
 
@@ -21,6 +23,19 @@ app.use(express.static('public'));
 // check route status
 app.get('/api/check', (req, res) => {
   res.status(200).json({ status: 'OK', message: 'Server is running properly' });
+});
+
+// Test database connection
+// This is a test route to check if the database connection is working
+// and to get the current server time
+app.get('/test-db', async (req, res) => {
+  try {
+    const result = await pool.query('select NOW()');
+    res.send(`DB Connected. Server time: ${result.rows[0].now}`);
+  } catch(err) {
+    console.error('DB Connection Error:', err);
+    res.status(500).send('DB Connection Error');
+  };
 });
 
 // Use routes (to be added later)
