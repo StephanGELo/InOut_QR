0676a28aa87be6a5bb390d11a32a11e268b53d2a
Stephan Lefort <lefortsge@gmail.com>
2025-05-02 10:31:09 -0700

    Create a post route to add new employee's details into the database

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -3,10 +3,22 @@ import bcrypt from 'bcrypt';
 import pool from '../config/db.js';
 
 const router = express.Router();
+const saltRounds = 15;
 
 // Testing route
 router.get('/test', (req, res) =>{
   res.send("Admin API is working");
+});
+
+// Get all admins
+router.get('/employees', async(req, res) => {
+  try {
+    const query = "SELECT id, name, email FROM users WHERE role ='employee' ORDER BY id ASC";
+    const result = await pool.query(query);
+    res.status(200).json(result.rows);
+  } catch(err) {
+    res.status(500).json({error: 'Failed to fetch employees'});
+  };
 });
 
 // Admin login route
@@ -32,15 +44,31 @@ router.post('/login', async (req, res) => {
   };
 });
 
-// Get all admins
-router.get('/employees', async(req, res) => {
+// Add new employee
+router.post('/employees', async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(400).json({ error: "Name, email and password are requred"});
+  }
+
   try {
-    const query = "SELECT id, name, email FROM users WHERE role ='employee' ORDER BY id ASC";
-    const result = await pool.query(query);
-    res.status(200).json(result.rows);
-  } catch(err) {
-    res.status(500).json({error: 'Failed to fetch employees'});
+    // check if user already exists
+    const checkUserQuery = 'SELECT * FROM users WHERE email = $1';
+    const checkUserResult = await pool.query(checkUserQuery, [email]);
+    if (checkUserResult.rows.length > 0) {
+      return res.status(400).hjson({ error: "User already exists."});
+    } 
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    // Insert new user into the database
+    const insertUserQuery = 'INSERT INTO users (name, email, password, role) VALUES ($1, $2, $3, $4) RETURNING id, email';
+    const insertUserResult = await pool.query(insertUserQuery, [name, email, hashedPassword, 'employee']);
+    res.status(201).json(insertUserResult.rows[0]);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to create employee"})
   };
 });
+
 
 export default router;
