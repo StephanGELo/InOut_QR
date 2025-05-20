bc5d691df52e12ffe2b5210efa8f529ee0664cbe
Stephan Lefort <lefortsge@gmail.com>
2025-05-19 19:44:38 -0700

    Fix bug for deployment

--- client/src/components/AdminLogin.jsx
+++ client/src/components/AdminLogin.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import "../styles/login.css";
+import "../styles/Login.css";
 
 function AdminLogin() {
     const [ email, setEmail ] = useState("");
