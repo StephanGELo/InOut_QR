a3eb2b5d9bed65dee2ff131761748a3e1d138c0e
Stephan Lefort <lefortsge@gmail.com>
2025-05-21 16:00:00 -0700

    Install Tailwindcss and configure vite.config.js to implement tailwindcss to all the components of the project

--- client/src/components/EmployeeLogin.jsx
+++ client/src/components/EmployeeLogin.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import '../styles/EmployeeLogin.css';
+// import '../styles/EmployeeLogin.css';
 
 
 function EmployeeLogin() {
@@ -12,12 +12,12 @@ function EmployeeLogin() {
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
 
-    const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000';
+    const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
     const handleLogin = async (e) => {
         e.preventDefault();
         try{
-            const result = await fetch(`${BASE_URL}/api/employee/login`, {
+            const result = await fetch(`${API_BASE_URL}/employee/login`, {
                 method: "POST",
                 headers: {
                     "Content-Type" : "application/json",
@@ -46,54 +46,60 @@ function EmployeeLogin() {
     }
 
     return (
-        <div className='employee-login'>
-            <header>
-                <Link to="/">
-                    <img src="/assets/logo.png" alt="InOut QR Logo" className="logo" />
-                </Link>
-                <h1>Welcome to InOut QR.</h1>
-                <p>Please login to track your attendance</p> 
-            </header>
-            <form onSubmit={handleLogin} className='login-form'>
-                <label htmlFor='email'>Email</label>
-                <input 
-                    type="text"
-                    value={email}
-                    id="email"
-                    name="email"
-                    autoComplete="email"
-                    onChange={(e)=> setEmail(e.target.value)}
-                    required
-                />
-
-                <label htmlFor='password'>Password</label>
-                <input 
-                    type="password"
-                    id="password"
-                    value={password}
-                    name="password"
-                    autoComplete="current-password"
-                    onChange={(e) => setPassword(e.target.value) }
-                    required
-                />
-
-                <button type="submit" className="login-button">Login</button>
-                <div className="login-options">
-                    <input type="checkbox" id="remember" name="remember"/>
-                    <label htmlFor='remember'>Remember Me</label>
-                    <Link to="/forgot-password" className="forgot-password">Forgot Password</Link>
-                </div>
-            </form>
-
-            <footer>
-                <Link to="/support">Contact Support</Link> | 
-                <Link to="/terms">Terms of Service</Link> | 
-                <Link to="/privacy">Privacy Policy</Link>
-            </footer>
-
+        <div>
+            <h1 className="text-2xl font-bold !text-red-600">Tailwind is working</h1>
+            <div className="bg-red-100 p-4 text-center rounded-lg">
+                This is red is working!
+            </div>
         </div>
-    )
+    );
 
+        // <div className='employee-login'>
+        //     <header>
+        //         <Link to="/">
+        //             <img src="/assets/logo.png" alt="InOut QR Logo" className="logo" />
+        //         </Link>
+        //         <h1>Welcome to InOut QR.</h1>
+        //         <p>Please login to track your attendance</p> 
+        //     </header>
+        //     <form onSubmit={handleLogin} className='login-form'>
+        //         <label htmlFor='email'>Email</label>
+        //         <input 
+        //             type="text"
+        //             value={email}
+        //             id="email"
+        //             name="email"
+        //             autoComplete="email"
+        //             onChange={(e)=> setEmail(e.target.value)}
+        //             required
+        //         />
+
+        //         <label htmlFor='password'>Password</label>
+        //         <input 
+        //             type="password"
+        //             id="password"
+        //             value={password}
+        //             name="password"
+        //             autoComplete="current-password"
+        //             onChange={(e) => setPassword(e.target.value) }
+        //             required
+        //         />
+
+        //         <button type="submit" className="login-button">Login</button>
+        //         <div className="login-options">
+        //             <input type="checkbox" id="remember" name="remember"/>
+        //             <label htmlFor='remember'>Remember Me</label>
+        //             <Link to="/forgot-password" className="forgot-password">Forgot Password</Link>
+        //         </div>
+        //     </form>
+
+        //     <footer>
+        //         <Link to="/support">Contact Support</Link> | 
+        //         <Link to="/terms">Terms of Service</Link> | 
+        //         <Link to="/privacy">Privacy Policy</Link>
+        //     </footer>
+
+        // </div>
 }
 
 export default EmployeeLogin;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './components/App.jsx';
+import './styles/index.css';
 
 const root = createRoot(
     document.getElementById('root')
