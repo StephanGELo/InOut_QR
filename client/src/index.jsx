f1f1cbff59898aef0c3af38bd28a9a7cbf369d4b
Stephan Lefort <lefortsge@gmail.com>
2025-05-28 16:29:17 -0700

    Fix bug during deployment

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './components/App.jsx';
-import './styles/index.css';
+// import './styles/index.css';
 
 const root = createRoot(
     document.getElementById('root')
